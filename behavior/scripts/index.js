28e78a7894148b14622849e316792c095a20f6a7
teroqim <teroqim@gmail.com>
2016-12-28 01:03:03 +0100

    add streams for weather

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -33,6 +33,29 @@ exports.handle = (client) => {
     }
   })
 
+  const collectCity = client.createStep({
+    satisfied() {
+      return Boolean(client.getConversationState().weatherCity)
+    },
+
+    prompt() {
+      // Need to prompt user for city
+      console.log('Need to ask user for city')
+      client.done()
+    },
+  })
+
+  const provideWeather = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt() {
+      // Need to provide weather
+      client.done()
+    },
+  })
+
   client.runFlow({
     classifications: {
       // map inbound message classifications to names of streams
@@ -41,9 +64,9 @@ exports.handle = (client) => {
       // configure responses to be automatically sent as predicted by the machine learning model
     },
     streams: {
-      main: 'onboarding',
-      onboarding: [sayHello],
-      end: [untrained],
+      main: 'getWeather',
+      hi: [sayHello],
+      getWeather: [collectCity, provideWeather],
     },
   })
 }
